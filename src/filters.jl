9f950c5a8837678f6077327eb2050de7460afc73
Ryan Dungee <ryan.dungee@utoronto.ca>
2023-05-16 16:06:22 -0400

    Fixed the escape characters

--- src/filters.jl
+++ src/filters.jl
@@ -8,9 +8,9 @@ Uses the idea of Johansson and Gavel (1994), which is to create a "Gaussian whit
 After this shaping the inverse FFT produces a matrix which contains the turbulent phase screen with the proper statistics, in this case Kolmogorov.
 Specifically, the output of the inverse FFT will be a random field which has the following spatial power spectral density:
 
-``Phi_n\\left(\\kappa\\right) = 0.033 C_n^2 \\kappa^{-11/3}``
+``\\Phi_n\\left(\\vec{k}\\right) = 0.033 \\: C_n^2 \\: \\vec{k}^{-11/3}``
 
-where ``\\kappa = 2\\pi\\left(f_x\\hat{i} + f_y\\hat{j}\\right)``
+where ``\\vec{k} = 2\\pi\\left(f_x \\: \\hat{i} + f_y \\: \\hat{j}\\right)`` is the angular spatial frequency in radians/meter
 """
 function kolmogorovfilter(frequencies::AbstractVector{AbstractFloat},
                           Cn_squared::AbstractFloat,
